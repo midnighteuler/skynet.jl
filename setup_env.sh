495bf27a973e17f6d2220bf5c2005aa4fee95f4d
Michael Souza <pongosapien@knomsa.in>
2013-09-16 03:20:50 -0700

    Fixed requirements.

--- setup_env.sh
+++ setup_env.sh
@@ -2,6 +2,7 @@ git clone git://github.com/yyuu/pyenv.git .pyenv
 git clone git://github.com/yyuu/pyenv-virtualenv.git ./.pyenv/plugins/pyenv-virtualenv
 export PYENV_ROOT="`pwd`/.pyenv"
 export PATH="`pwd`/.pyenv/bin:$PATH"
+export CONFIGURE_OPTS="--enable-shared"
 eval "$(pyenv init -)"
 pyenv install 2.7.5
 pyenv global 2.7.5
